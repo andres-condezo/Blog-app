c471496d94bdd02aa85afbce34d312270b0abfca
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-09 13:10:22 -0500

    Fix(db/migrate): [Fix migrations file]

--- db/migrate/20220709090327_create_comments.rb
+++ db/migrate/20220709090327_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :user, null: false, foreign_key: true
+      t.references :author, index: true, foreign_key: { to_table: :users, on_delete: :cascade, null: false }
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20220709090341_create_likes.rb
+++ db/migrate/20220709090341_create_likes.rb
@@ -1,7 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :author, index: true, foreign_key: { to_table: :users, on_delete: :cascade, null: false }
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
