fac7e411f1476ee4857df04c346261daac56ea6e
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-09 11:50:29 -0500

    Feat(models/): [Create comment model]

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
+end
