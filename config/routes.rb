6eb10859dcaae49b4ec8f5e085723521477b3b00
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-13 17:38:35 -0500

    Feat(config/routes.rb): [Create users, posts, and index routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
+  ge  # Defines the root path route ("/")
+  root to: 'users#index'
 end
