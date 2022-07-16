ee8d5948be36ddd572ede67b63ca6e2c2f90abad
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-16 18:42:27 -0500

    Feat(views/posts): [Add comments and likes resources]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
-
+  
   # Defines the root path route ("/")
   root to: 'users#index'
 end
