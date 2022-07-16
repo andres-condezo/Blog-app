695224214496c1961cc6c332e5f50a323b69ec81
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-16 18:38:59 -0500

    Feat(views/partials): [Create new, create, and post_params actions]

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,30 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @user_posts = @user.posts.order('created_at DESC')
   end
 
   def show
-    @post = Post.find(params[:id])
+    @current_post = Post.find(params[:id])
+    @current_user = current_user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
