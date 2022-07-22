3b5b59a12ebba7edb4bcf1eb9d586bc12680773e
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-22 09:44:21 -0500

    Feat: [Set up devise gem]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,14 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-  end
+  # config.after_initialize do
+  #   Bullet.enable        = true
+  #   Bullet.alert         = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console       = true
+  #   Bullet.rails_logger  = true
+  #   Bullet.add_footer    = true
+  # end
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -45,9 +45,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
