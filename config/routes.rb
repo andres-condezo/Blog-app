500917ce48326e642187e398436c4b77f02780ba
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-23 13:01:40 -0500

    Feat(config/routes): [Create api routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  post '/login', to: 'auth#login', default: {:format => :json} 
   # Defines the root path route ("/")
   root to: 'users#index'
+
+  scope 'api' do
+    get '/posts/:id/comments' => 'api/comments#index', as: :api_post_comments
+    post '/posts/:id/comments' => 'api/comments#create', as: :api_post_comments_create
+    resources :users, only: [] do
+      get '/posts' => 'api/posts#index', as: :api_user_posts
+    end
+  end 
 
   devise_for :users
 
@@ -11,4 +20,5 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
 end
