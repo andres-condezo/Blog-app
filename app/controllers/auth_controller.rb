d50d071ceea84c6aff46974da38a77d0c37bc913
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-23 14:36:42 -0500

    Feat(controllers/api): [create sign up endpoint]

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -10,6 +10,31 @@ class AuthController < ApplicationController
     end
   end
 
+  def signup
+    user = User.new(user_params)
+
+    if user.save
+      render json: {
+        token: token(user.id),
+        user_id: user.id,
+        confirmation_url:
+        "#{confirmation_url(user)}?confirmation_token=#{user.confirmation_token}"
+      }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def logout
+    user = User.find_by('lower(email) = ?', user_params[:email].downcase)
+
+    if user&.authenticate(user_params[:password])
+      render json: { message: 'Logged out' }, status: :ok
+    else
+      render json: { errors: ['Sorry, incorrect email or password'] }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
