2bd5ae046147de410d646ffe5695c4ffa565e817
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-23 14:37:22 -0500

    Feat: [create new post and get comments api endpoints]

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   post '/login', to: 'auth#login', default: {:format => :json} 
+  post '/signup', to: 'auth#signup', default: {:format => :json}
+  post '/logout', to: 'auth#logout', default: {:format => :json} 
+
   # Defines the root path route ("/")
   root to: 'users#index'
 
   scope 'api' do
-    get '/posts/:id/comments' => 'api/comments#index', as: :api_post_comments
-    post '/posts/:id/comments' => 'api/comments#create', as: :api_post_comments_create
+    get 'user/:user_id/posts/:id/comments' => 'api/comments#index', as: :api_post_comments
+    post 'user/:user_id/posts/:id/comments' => 'api/comments#create', as: :api_post_comments_create
     resources :users, only: [] do
       get '/posts' => 'api/posts#index', as: :api_user_posts
     end
   end 
-
-  devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
