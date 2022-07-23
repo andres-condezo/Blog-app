a15c51e589d21235811f82c3bdd529a7930db283
buhariaminat <buhariaminat@yahoo.com>
2022-07-23 19:15:23 +0100

    Add comments controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,22 @@
+class Api::CommentsController < ApplicationController
+    before_action :require_login
+    skip_before_action :verify_authenticity_token
+    def index
+      comments = Comment.where(post_id: params[:id])
+      render json: { comments: }, status: :ok
+    end
+    def create
+      comment = Comment.new(comment_params)
+      comment.author_id = current_user_id
+      comment.post_id = params[:id]
+      if comment.save
+        render json: { comment: }, status: :ok
+      else
+        render json: { errors: comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+    private
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
