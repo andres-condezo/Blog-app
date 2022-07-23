67d10dcab5aa1d1b67831f80803e41b5052a638f
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-23 13:00:33 -0500

    Feat(controllers/application_controller.rb): [Update application controller file]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,35 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  def token(user_id)
+    payload = { user_id: }
+    JWT.encode(payload, hmac_secret, 'HS256')
+  end
+
+  def hmac_secret
+    ENV.fetch('API_SECRET_KEY', nil)
+  end
+
+  def client_has_valid_token?
+    !!current_user_id
+  end
+
+  def current_user_id
+    begin
+      token = request.headers['Authorization']
+      decoded_array = JWT.decode(token.split[1], hmac_secret, true, { algorithm: 'HS256' })
+      payload = decoded_array.first
+    rescue StandardError # JWT::VerificationError
+      return nil
+    end
+    payload['user_id']
+  end
+
+  def require_login
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless client_has_valid_token?
+  end
 
   protected
 
